2b6f4ede43e7407c22aedb4d21a2b0d6267ae9c6
Vivek Chowdhury <vivek.chowdhury@nagarro.com>
2020-07-18 22:15:22 +0530

    unused file deleted from repository

--- src/api/ProductService.js
+++
@@ -1,9 +0,0 @@
-import { handleError, handleResponse } from "./serviceUtils";
-const baseUrl = process.env.REACT_APP_API_URL + "/products";
-
-/**
- * @description This function is responsible for fetching list of products from server.
- */
-export function loadProducts() {
-  return fetch(baseUrl).then(handleResponse).catch(handleError);
-}
